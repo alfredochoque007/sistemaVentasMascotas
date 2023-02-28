14ae911869ecc8ec96642514f0f2645dd503cd2c
Luis <alfredochoque.c@gmail.com>
2023-02-28 14:24:36 -0400

    inicio del proyecto

---
+++ src/main/java/CapaPresentacion/main.java
@@ -0,0 +1,14 @@
+
+package CapaPresentacion;
+
+import CapaPresentacion.Principal;
+
+
+public class main {
+       public static void main(String[] args) {
+        
+           Principal ventana = new Principal();
+           ventana.setVisible(true);
+           
+    }
+}
